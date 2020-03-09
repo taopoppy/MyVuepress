d52a4cd0b2b829d12f34214a602e024f85e9f555
taopoppy <15009571633@163.com>
2020-03-09 17:17:05 +0800

    add node-react blockchain

--- docs/.vuepress/sidebars/nodeblockchain.js
+++ docs/.vuepress/sidebars/nodeblockchain.js
@@ -4,31 +4,32 @@ module.exports = [
   {
     title:'区块链概念科普',
     children: [
+      '/node-blockchain/nodeblock_one_introduce'
     ]
-	},
-	{
+  },
+  {
     title:'Nodejs开发一个区块链',
     children: [
     ]
-	},
-	{
+  },
+  {
     title:'以太坊智能合约Solidity',
     children: [
     ]
-	},
-	{
+  },
+  {
     title:'Truffle+Web3.js+React开发智能合约',
     children: [
     ]
-	},
-	{
+  },
+  {
     title:'IPFS星际文件系统',
     children: [
     ]
-	},
-	{
+  },
+  {
     title:'实战-开发区块链版本的慕课',
     children: [
     ]
-	}
+  }
 ]
