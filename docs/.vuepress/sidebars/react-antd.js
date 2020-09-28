058fcfdddf385dada6503e88d90d3f0626562fab
Administrator <15009571633@163.com>
2020-09-28 23:14:36 +0800

    css continue

--- docs/.vuepress/sidebars/react-antd.js
+++ docs/.vuepress/sidebars/react-antd.js
@@ -2,9 +2,12 @@
 module.exports = [
   '/react-antd/',
   {
-    title:'CSS基础',
+    title:'CSS全集',
     children: [
-			'/react-antd/css-html-base-one'
+      '/react-antd/css-html-base-one',
+      '/react-antd/css-html-andvance-one',
+      '/react-antd/css-html-skill-one',
+      '/react-antd/css-html-combat-one'
     ]
 	},
 ]
