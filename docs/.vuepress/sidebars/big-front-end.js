24b0feb901efc090bfefbd46c90fdfce5a1d4b42
taopoppy <15009571633@163.com>
2021-03-23 21:51:42 +0800

    start front-end projectable

--- docs/.vuepress/sidebars/big-front-end.js
+++ docs/.vuepress/sidebars/big-front-end.js
@@ -6,7 +6,8 @@ module.exports = [
       '/Big-Front-End/start-big-front-end-1-1-1',
       '/Big-Front-End/start-big-front-end-1-1-2',
       '/Big-Front-End/start-big-front-end-1-2-1',
-      '/Big-Front-End/start-big-front-end-1-2-4'
+      '/Big-Front-End/start-big-front-end-1-2-4',
+      '/Big-Front-End/start-big-front-end-1-3-1'
     ]
   }
 ]
