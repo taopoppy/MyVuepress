6d8a57fb47f8dd8b859c5b507a3b269f13fd423f
taopoppy <15009571633@163.com>
2022-01-19 10:59:51 +0800

    start vue3

--- docs/.vuepress/sidebars/vueInterview.js
+++ docs/.vuepress/sidebars/vueInterview.js
@@ -2,8 +2,10 @@
 module.exports = [
   '/vue-interview/',
   {
-    title: 'Vue使用',
+    title: 'Vue基础语法',
     children: [
+      '/vue-interview/vue-base-one',
+      '/vue-interview/vue-base-two',
     ]
 	},
 ]
